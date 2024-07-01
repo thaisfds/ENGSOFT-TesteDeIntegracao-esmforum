befb624cd3822c55ef831f509d33e9dab31aec89
Thaís Ferreira <thaisfdasilva159@gmail.com>
2024-07-01 16:00:08 -0300

    Teste com cobertura de 100%

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,38 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+/*MEUS TESTES PARA A ATIVIDADE*/
+
+//Cadastro de 3 respostas
+test('Testando cadastro de três respostas', () => {
+  // Cadastro de uma pergunta para associar respostas
+  const idPergunta = modelo.cadastrar_pergunta('2+2?');
+
+  // Cadastro de respostas para a pergunta
+  modelo.cadastrar_resposta(idPergunta, '2');
+  modelo.cadastrar_resposta(idPergunta, '4');
+  modelo.cadastrar_resposta(idPergunta, '6');
+
+  // Recuperação de respostas para a pergunta
+  const respostas = modelo.get_respostas(idPergunta);
+
+  // Verificação das respostas
+  expect(respostas.length).toBe(3); // Espera 3 respostas
+  expect(respostas[0].texto).toBe('2');
+  expect(respostas[1].texto).toBe('4');
+  expect(respostas[2].texto).toBe('6');
+});
+
+//Teste do get_pergunta
+test('Testando pegar a pergunta', () => {
+  const idPergunta = modelo.cadastrar_pergunta('Qual é a capital do Brasil?');
+
+  const pergunta = modelo.get_pergunta(idPergunta);
+
+  expect(pergunta).toEqual({
+    id_pergunta: idPergunta,
+    texto: 'Qual é a capital do Brasil?',
+    id_usuario: 1
+  });
+});
